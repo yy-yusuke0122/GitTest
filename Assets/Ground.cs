6da55178653c4170da6015fc3b9c558307139a67
yy-yusuke0122 <61263724+yy-yusuke0122@users.noreply.github.com>
2020-05-29 12:01:51 +0900

    test

---
+++ Assets/Ground.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float H = Input.GetAxis("Horizontal");
+        float V = Input.GetAxis("Vertical");
+        transform.Rotate(V, 0, -H);
+    }
+}
---
+++ Assets/MoveWall.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWall : MonoBehaviour
+{
+    [SerializeField]
+    float moveVal;
+    [SerializeField]
+    float moveSpeed;
+    bool moveFront;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveFront = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(moveFront)
+            transform.Translate(0, 0, moveSpeed);
+        else
+            transform.Translate(0, 0, -moveSpeed);
+
+        if (this.transform.position.z >= moveVal)
+        {
+            moveFront = false;
+        }
+        else if (this.transform.position.z <= -moveVal)
+        {
+            moveFront = true;
+        }
+    }
+}
